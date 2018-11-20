a81f2126c3a9788f0265f0280df1c3ff37bee8a1
Andrey Belykh <abelykh@yahoo.ca>
2018-11-20 00:17:54 -0500

    Incremental

--- Main/Emulator/BkScreen.cpp
+++ Main/Emulator/BkScreen.cpp
@@ -16,22 +16,22 @@ namespace bk
 uint8_t _palette1[4] = { Black, Blue, Green, Red };
 uint8_t _palette2[4] = { Black, LightGrey, Grey, White };
 uint32_t _palette3[16] = {
-		Black | Black << 8 | Black << 16 | Black << 24,
-		Black | Black << 8 | Black << 16 | White << 24,
-		Black | Black << 8 | White << 16 | Black << 24,
-		Black | Black << 8 | White << 16 | White << 24,
-		Black | White << 8 | Black << 16 | Black << 24,
-		Black | White << 8 | Black << 16 | White << 24,
-		Black | White << 8 | White << 16 | Black << 24,
-		Black | White << 8 | White << 16 | White << 24,
-		White | Black << 8 | Black << 16 | Black << 24,
-		White | Black << 8 | Black << 16 | White << 24,
-		White | Black << 8 | White << 16 | Black << 24,
-		White | Black << 8 | White << 16 | White << 24,
-		White | White << 8 | Black << 16 | Black << 24,
-		White | White << 8 | Black << 16 | White << 24,
-		White | White << 8 | White << 16 | Black << 24,
-		White | White << 8 | White << 16 | White << 24,
+		Black << 24 | Black << 16 | Black << 8 | Black,
+		Black << 24 | Black << 16 | Black << 8 | White,
+		Black << 24 | Black << 16 | White << 8 | Black,
+		Black << 24 | Black << 16 | White << 8 | White,
+		Black << 24 | White << 16 | Black << 8 | Black,
+		Black << 24 | White << 16 | Black << 8 | White,
+		Black << 24 | White << 16 | White << 8 | Black,
+		Black << 24 | White << 16 | White << 8 | White,
+		White << 24 | Black << 16 | Black << 8 | Black,
+		White << 24 | Black << 16 | Black << 8 | White,
+		White << 24 | Black << 16 | White << 8 | Black,
+		White << 24 | Black << 16 | White << 8 | White,
+		White << 24 | White << 16 | Black << 8 | Black,
+		White << 24 | White << 16 | Black << 8 | White,
+		White << 24 | White << 16 | White << 8 | Black,
+		White << 24 | White << 16 | White << 8 | White
 };
 uint16_t _palette4[4] = {
 		LightBlue | LightBlue << 8,
@@ -59,7 +59,7 @@ void BkScreen::InvertColor()
 {
 }
 
-//__attribute__((section(".ramcode")))
+__attribute__((section(".ramcode")))
 Rasterizer::RasterInfo BkScreen::rasterize(
 		unsigned cycles_per_pixel, unsigned line_number, Pixel *target)
 {
@@ -67,7 +67,7 @@ Rasterizer::RasterInfo BkScreen::rasterize(
 	bool isNarrow = (RamBuffer[0x0020] == 0);
 	uint8_t divider = (isNarrow ? 1 : 2);
 
-	unsigned scaledLine = (line_number - this->_startLine) / 2;
+	uint16_t scaledLine = (line_number - this->_startLine) / 2;
 	if (scaledLine == 0)
 	{
 		this->_frames++;
@@ -81,15 +81,19 @@ Rasterizer::RasterInfo BkScreen::rasterize(
 	else
 	{
 		// Border to the left
-		memset(&target[0], borderColor, this->_horizontalBorder / divider);
+		//memset(&target[0], borderColor, this->_horizontalBorder / divider);
 
-		uint16_t vline = scaledLine - this->_verticalBorder;
+		uint8_t vline = scaledLine - this->_verticalBorder;
+
+		// scroll
+		vline += (port0177664 - 0330);
+
 		uint32_t* bitmap = (uint32_t*)this->GetPixelPointer(vline);
 		uint8_t* dest = &target[this->_horizontalBorder / divider];
 
 		if (isNarrow)
 		{
-			for (int x = 0; x < 2; x++)
+			for (int x = 0; x < 16; x++)
 			{
 				Draw4BW(*bitmap, _palette3, dest);
 				dest += 32;
@@ -107,8 +111,8 @@ Rasterizer::RasterInfo BkScreen::rasterize(
 		}
 
 		// Border to the right
-		memset(&target[(this->_hResolution - this->_horizontalBorder) / divider],
-				borderColor, this->_horizontalBorder / divider);
+		//memset(&target[(this->_hResolution - this->_horizontalBorder) / divider],
+		//		borderColor, this->_horizontalBorder / divider);
 	}
 
 	Rasterizer::RasterInfo result;
